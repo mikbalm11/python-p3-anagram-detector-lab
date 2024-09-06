28115f616e2cc634bc953cc027914bfb6f9bbe90
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-05 23:50:50 -0400

    passes all tests!

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    '''A class to find anagrams of a given word.'''
+
+    def __init__(self, word):
+        '''Initialize with a word.'''
+        self.word = word
+
+    def match(self, words):
+        '''Find all anagrams of the given word in the list.'''
+        matchlist = []
+        for word in words:
+            if sorted(self.word.lower()) == sorted(word.lower()):
+                matchlist.append(word)
+        return matchlist
